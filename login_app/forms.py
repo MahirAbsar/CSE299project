0d84ec87d4dfc1a0b01d24f6e09438e5709e99a1
jannatul ferdaous <jannatul@gmail.com>
2021-08-24 20:04:06 +0600

    Added Email Authentication and Complete Login System

--- login_app/forms.py
+++ login_app/forms.py
@@ -1,9 +1,26 @@
 from django import forms
-from django.contrib.auth.forms import  UserCreationForm , AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UserChangeForm
 from django.contrib.auth.models import User
+from .import models
+
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'abc@northsouth.edu'}))
+    email = forms.EmailField(widget=forms.TextInput(
+        attrs={'placeholder': 'abc@northsouth.edu'}))
+
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('first_name', 'last_name', 'username',
+                  'email', 'password1', 'password2',)
+
+
+class UserInfoChange(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email', 'password')
+
+
+class ProfilePicForm(forms.ModelForm):
+    class Meta:
+        model = models.userInfo
+        fields = ('profile_pic',)
