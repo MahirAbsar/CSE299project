fc85f18042c5c732fbd41ecc3eef5861cce08d9b
Jannatul-Ferdaous-1731 <80451777+Jannatul-Ferdaous-1731@users.noreply.github.com>
2021-08-29 20:00:33 +0600

    Final blog create, update and details added

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
 
     path('write/', views.CreateBlog.as_view(), name="write"),
     path('blog_list/', views.BlogList.as_view(), name="blog_list"),
-    path('blog_details/<slug:slug>', views.blog_details, name="blog_details"),
+    path('blog_details/<slug>', views.blog_details, name="blog_details"),
     path('my_blogs/', views.MyBlogs.as_view(), name="my_blogs"),
     path('edit_blogs/<int:pk>', views.UpdateBlogs.as_view(), name="edit_blogs"),
 ]
